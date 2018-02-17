c2a3e288862ecfca47041b8c778f21dde4a1c681
mishingo <miguel@localstaffingllc.com>
2018-02-17 12:54:49 -0700

    get metamask to open

---
+++ dapp-module/StrikeContract/index.js
@@ -0,0 +1,230 @@
+
+const StrikeContractArtifacts = require('../../build/contracts/StrikeContract.json')
+
+const Web3 = require('web3')
+const BN = Web3.utils.BN
+const ZeroClientProvider = require('web3-provider-engine/zero.js')
+
+class StrikeContract {
+  constructor (options) {
+
+    this.StrikeContract = null
+
+    this.pollingInterval = null
+    this.account = null
+    this.unlocked = false
+    this.balanceWei = 0
+    this.balance = 0
+    this.address = '0x345ca3e014aaf5dca488057592ee47305d9b3e10'
+    this.genesisBlock = 0
+    this.loading = false
+    this.options = {
+      readonlyRpcURL: 'http://localhost:9545',
+      autoInit: true,
+      getPastEvents: false,
+      watchFutureEvents: false,
+      connectionRetries: 3
+    }
+    Object.assign(this.options, options)
+    if (this.options.autoInit) this.initWeb3()
+  }
+
+  // hello world : )
+  helloWorld () {
+    console.log('hello world!')
+  }
+
+  /*
+   * Connect
+   */
+
+  initWeb3 () {
+    return new Promise((resolve, reject) => {
+
+      let web3Provider = true
+
+        // check for metamask
+        if (global.web3) {
+          web3Provider = web3.currentProvider
+          // attempt to try again if no web3Provider
+        } else if (this.options.connectionRetries > 0){
+          this.options.connectionRetries -= 1
+          setTimeout(() => {
+            this.initWeb3().then(resolve).catch((error) => {
+              reject(new Error(error))
+            })
+          }, 1000)
+          // revert to a read only version using infura endpoint
+        } else {
+          this.readOnly = true
+          web3Provider = ZeroClientProvider({
+            getAccounts: function(){},
+            rpcUrl: this.options.readonlyRpcURL
+          })
+        }
+
+        if (web3Provider) {
+          global.web3 = new Web3(web3Provider)
+          this.startChecking()
+
+          if (this.options.getPastEvents) this.getPastEvents()
+          if (this.options.watchFutureEvents) this.watchFutureEvents()
+        }
+      })
+    }
+  
+
+  /*
+   * Check every second for switching network or switching wallet
+   */
+
+  startChecking () {
+    if (this.pollingInterval) clearInterval(this.pollingInterval)
+    this.getGenesisBlock()
+    .then(() => {
+      this.pollingInterval = setInterval(this.check.bind(this), 1000)
+    })
+    .catch((err) => {
+      throw new Error(err)
+    })
+  }
+
+  check () {
+    this.checkNetwork()
+    .then(this.checkAccount.bind(this))
+    .catch((error) => {
+      console.error(error)
+      throw new Error(error)
+    })
+  }
+
+  checkNetwork () {
+    return global.web3.eth.net.getId((err, netId) => {
+      if (err) console.error(err)
+      if (!err && this.network !== netId) {
+        this.network = netId
+        return this.deployContract()
+      }
+    })
+  }
+
+  deployContract () {
+    if (!this.address || this.address === 'REPLACE_WITH_CONTRACT_ADDRESS') return new Error('Please provide a contract address')
+    this.StrikeContract = new global.web3.eth.Contract(StrikeContractArtifacts.abi, this.address)
+  }
+
+  checkAccount () {
+    return global.web3.eth.getAccounts((error, accounts) => {
+      if (error) throw new Error(error)
+      if (accounts.length && this.account !== accounts[0]) {
+        this.unlocked = true
+        this.account = accounts[0]
+      } else if (!accounts.length) {
+        this.unlocked = false
+        this.account = null
+      }
+    })
+  }
+
+
+  /*
+   * Not Yet Implemented vvvv
+   */
+
+  getGenesisBlock () {
+    return new Promise((resolve, reject) => {
+      resolve()
+    })
+  }
+
+  getPastEvents () {
+    return new Promise((resolve, reject) => {
+      resolve()
+    })
+  }
+
+  watchFutureEvents () {
+    return new Promise((resolve, reject) => {
+      resolve()
+    })
+  }
+
+
+
+
+  /*
+   *
+   * Constant Functions
+   *
+   */
+
+  getTotal () {
+    return this.StrikeContract.methods.getTotal().call()
+      .then((resp) => {
+      console.log(resp)
+      return resp
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+  getVote () {
+    return this.strikeContract.methods.getVote().call()
+      .then((resp) => {
+      console.log(resp)
+      return resp
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+
+  /*
+   *
+   * Transaction Functions
+   *
+   */
+
+  increaseVote () {
+    if (!this.account) return Promise.reject(new Error('Unlock Account'))
+    return this.StrikeContract.methods.increaseVote().send({from: this.account})
+    .on('transactionHash', (hash) => {
+      console.log(hash)
+      this.loading = true
+    })
+      .then((resp) => {
+      this.loading = false
+      console.log(resp)
+      return resp
+    }).catch((err) => {
+      this.loading = false
+      console.error(err)
+    })
+  }
+  decreaseVote () {
+    if (!this.account) return Promise.reject(new Error('Unlock Account'))
+    return this.strikeContract.methods.decreaseVote().send({from: this.account})
+    .on('transactionHash', (hash) => {
+      console.log(hash)
+      this.loading = true
+    })
+      .then((resp) => {
+      this.loading = false
+      console.log(resp)
+      return resp
+    }).catch((err) => {
+      this.loading = false
+      console.error(err)
+    })
+  }
+
+  /*
+   *
+   * Events
+   *
+   */
+
+
+
+
+}
+
+module.exports = StrikeContract
--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,36 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <button @click="getTotal"> Click Me to get total </button>
+    <button @click="increaseVote"> Click Me to vote </button>
   </div>
 </template>
 
 <script>
+const StrikeContract = require('../dapp-module/StrikeContract')
+let strikeContract = new StrikeContract()
+console.log(strikeContract)
+
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      vote: 1,
+      voteTotal: 1
+    }
+  },
+  methods: {
+    getTotal () {
+      return strikeContract.getTotal().then((total) => {
+        return this.total
+      })
+    },
+    increaseVote () {
+      return strikeContract.increaseVote().then((vote) => {
+        return this.getTotal()
+      })
+    }
+  }
 }
 </script>
 
